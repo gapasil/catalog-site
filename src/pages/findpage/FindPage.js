d9e0744eb7b823c884756ed243c65aa786265b69
vbxx0 <152014127+vbxx0@users.noreply.github.com>
2023-11-25 18:28:36 +0400

    Update FindPage.js
    Fixed a problem with copy while not secure origin

--- src/pages/findpage/FindPage.js
+++ src/pages/findpage/FindPage.js
@@ -140,15 +140,14 @@ export const FindPage = () => {
     const removeView = () => {
         setViewTarget(false)
     }
-
+    const unsecuredCopyToClipboard = (text) => { const textArea = document.createElement("textarea"); textArea.value=text; document.body.appendChild(textArea); textArea.focus();textArea.select(); try{document.execCommand('copy')}catch(err){console.error('Unable to copy to clipboard',err)}document.body.removeChild(textArea)};
+    
     function copyBuffer() {
-        navigator.clipboard.writeText(viewTarget._id)
-        .then(() => {
-            console.log('Text copied to clipboard');
-        })
-        .catch(err => {
-            console.error('Error in copying text: ', err);
-        });
+        if (window.isSecureContext && navigator.clipboard) {
+            navigator.clipboard.writeText(viewTarget._id);
+         } else {
+            unsecuredCopyToClipboard(viewTarget._id);
+         };
     }
 
     return (
